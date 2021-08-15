768e673cb4b3febe79e14f086dfb6136f0971e1c
Ryo Hilmi <ryodm.74@gmail.com>
2021-08-15 13:04:13 +0700

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="logo.png" />
+        <h1>ICHIRO ITS</h1>
+      </nav>
+    </header>
+  );
+}
